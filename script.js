3ab9d72be3dcb3687bcd8018283a6dc8f7db7454
Zaed Salah <borak.noor.3rd@gmail.com>
2025-05-09 15:36:26 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+/* show navbar */
+function toggleMenu() 
+{
+  const menu = document.getElementById('mobileMenu');
+  const overlay = document.getElementById('overlay');
+
+  const isOpen = !menu.classList.contains('translate-x-full');
+  if (isOpen) {
+    menu.classList.add('translate-x-full');
+    overlay.classList.add('hidden');
+  } else {
+    menu.classList.remove('translate-x-full');
+    overlay.classList.remove('hidden');
+  }
+}
